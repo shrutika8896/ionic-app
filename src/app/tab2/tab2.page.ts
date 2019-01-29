0d66f7fca13c58013b012843ad2fd7f1fe829437
Shrutika Patil <shrutika.patil@neosofttech.com>
2019-01-29 16:44:27 +0530

    QR code scanner

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,40 @@
 import { Component } from '@angular/core';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  scannedText: string;
+  constructor(private qrScanner: QRScanner) {
+  }
+
+  scanQRCode() {
+    this.qrScanner.prepare()
+  .then((status: QRScannerStatus) => {
+     if (status.authorized) {
+       // camera permission was granted
+       // start scanning
+       const scanSub = this.qrScanner.scan().subscribe((text: string) => {
+         console.log('Scanned something', text);
+         this.scannedText = text;
+         this.qrScanner.hide(); // hide camera preview
+         scanSub.unsubscribe(); // stop scanning
+       });
+
+     } else if (status.denied) {
+       // camera permission was permanently denied
+       // you must use QRScanner.openSettings() method to guide the user to the settings page
+       // then they can grant the permission from there
+     } else {
+       // permission was denied, but not permanently. You can ask for permission again at a later time.
+     }
+  })
+  .catch((e: any) => console.log('Error is', e));
+
+  }
+
+}
