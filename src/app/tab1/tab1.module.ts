dbe8092a5f5b83f3c62b74b9b2527191aa0aef5c
Shrutika Patil <shrutika.patil@neosofttech.com>
2019-01-29 14:23:28 +0530

    First commit

---
+++ src/app/tab1/tab1.module.ts
@@ -0,0 +1,23 @@
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule ,ReactiveFormsModule} from '@angular/forms';
+import { Tab1Page } from './tab1.page';
+import { Stripe } from '@ionic-native/stripe/ngx';
+
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([{ path: '', component: Tab1Page }]),
+    ReactiveFormsModule
+  ],
+  providers: [
+    Stripe
+  ],
+  declarations: [Tab1Page]
+})
+export class Tab1PageModule {}
---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { Stripe, StripeCardTokenParams } from '@ionic-native/stripe/ngx';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  cardNumber: string;
+  expiryMonth: number;
+  expiryYear: number;
+  cvv: string;
+   card: StripeCardTokenParams = {
+    number: '', // 16-digit credit card number
+    expMonth: 0, // expiry month
+    expYear: 0, // expiry year
+    cvc: ''
+  };
+    constructor(private stripe: Stripe) {
+      console.log("constructor");
+    }
+
+    ngOnInit(){
+      console.log('init');
+    }
+
+  createCard() {
+    this.card.number = this.cardNumber;
+    this.card.expMonth = this.expiryMonth;
+    this.card.expYear = this.expiryYear;
+    this.card.cvc = this.cvv;
+    this.stripe.createCardToken(this.card).then(data => {
+      console.log(data);
+    }, err => {
+      console.log(err);
+    });
+
+  }
+
+}
